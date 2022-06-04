788e7cade03beb823ce7acd5b8f178d48c8cbf1f
jerripat <jerripatterson@gmail.com>
2022-06-03 21:51:50 -0500

    additions

--- features/Annotations/variables.ts
+++ features/Annotations/variables.ts
@@ -29,6 +29,34 @@ const logNumber: (i: number) => void = (i: number) => {
     console.log(i);
 };
 
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20 }';
+const coordinates = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) When we declare a variable on one line
+//  and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord = false;
+
+for (let i = 0; i < words.length; i++){
+    
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
+
 
 
 
